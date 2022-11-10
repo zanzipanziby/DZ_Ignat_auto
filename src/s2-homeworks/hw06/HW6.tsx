09c2a6f86507d03533712233fb2f477df5f4aaab
zanzipanziby <zanzipanziby@gmail.com>
2022-11-11 00:01:53 +0300

    lesson-6.1

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -18,7 +18,7 @@ const HW6 = () => {
         saveState<string>('hw6-editable-span-value', value)
     }
     const restore = () => {
-        saveState('hw6-editable-span-value', '')
+        saveState('hw6-editable-span-value', 'enter text...')
         // делают студенты
 
     }
