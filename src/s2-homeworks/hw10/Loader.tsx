de7d2dd670314bf36a2f6d03ede26b7ea62d48cb
zanzipanziby <zanzipanziby@gmail.com>
2023-01-27 18:18:16 +0300

    homework-10

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,4 @@
 import s from './Loader.module.css'
+import loader from '../../assets/loader/loader.svg'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => <div className={s.loader}><img src={loader} alt="loader"/></div>
