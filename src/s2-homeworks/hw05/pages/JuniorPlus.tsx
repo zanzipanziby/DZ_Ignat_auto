3c255154b43d84af26912169a8fec52f81e9537e
zanzipanziby <zanzipanziby@gmail.com>
2023-01-27 19:50:31 +0300

    homework-12

--- src/s2-homeworks/hw05/pages/JuniorPlus.tsx
+++ src/s2-homeworks/hw05/pages/JuniorPlus.tsx
@@ -9,7 +9,7 @@ function JuniorPlus() {
         <div id={'hw5-page-junior-plus'}>
             <HW10 />
             <HW11 />
-            {/*<HW12 />*/}
+            <HW12 />
             {/*<HW13 />*/}
         </div>
     )
--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -2,13 +2,16 @@ const initState = {
     themeId: 1,
 }
 
-export const themeReducer = (state = initState, action: any): any => { // fix any
+export const themeReducer = (state = initState, action: ChangeThemeIdACType):{themeId: number} => { // fix any
     switch (action.type) {
         // дописать
+        case "SET_THEME_ID":
+            return {...state,themeId: action.id}
 
         default:
             return state
     }
 }
 
-export const changeThemeId = (id: number): any => ({ type: 'SET_THEME_ID', id }) // fix any
+type ChangeThemeIdACType = ReturnType<typeof changeThemeId>
+export const changeThemeId = (id: number) => ({ type: 'SET_THEME_ID', id }as const) // fix any
