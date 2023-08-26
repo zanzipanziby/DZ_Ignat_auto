98225d6d4193a37561d269708ecc4ad1180d4bc1
ZanziPanziBY <zanzipanziby@gmail.com>
2023-08-26 22:34:33 +0300

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -10,6 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <React.StrictMode>
         {/*для дз 10*/}
+        
         <Provider store={store}>
             <App/>
         </Provider>
